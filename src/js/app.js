d3bd5459c5f873b8607bcd04cfb9d0ad61792d33
aplusiv <50slash50@gmail.com>
2023-10-03 21:06:09 +0300

    test problems

--- src/js/app.js
+++ src/js/app.js
@@ -6,3 +6,5 @@ const collapsibleElement = document.querySelector('.collapse');
 console.log(collapsibleElement.offsetHeight);
 
 const example = new Collapsible(link, collapsibleElement, 0);
+
+//     "test": "echo \"Error: no test specified\" && exit 1"
